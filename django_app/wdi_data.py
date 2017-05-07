4cee83e44e7f281262f66706a9bac8b1f7e18fe3
Haylee Fay Ham <hayleeham@gmail.com>
2017-05-06 22:23:35 -0500

    finally have the models correct

--- django_app/wdi_data.py
+++ django_app/wdi_data.py
@@ -16,27 +16,27 @@ data = pd.read_csv('django_app/fertility.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = FertilityRates(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
@@ -58,27 +58,27 @@ data = pd.read_csv('django_app/perworkergdp.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = PerWorkerGDP(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
@@ -100,27 +100,27 @@ data = pd.read_csv('django_app/primaryenrollmentrate.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = PrimaryEnrollmentRate(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
@@ -142,27 +142,27 @@ data = pd.read_csv('django_app/laborparticipationrate.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = LaborParticipationRate(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
@@ -184,27 +184,27 @@ data = pd.read_csv('django_app/literacyrate.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = LiteracyRate(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
@@ -226,27 +226,27 @@ data = pd.read_csv('django_app/workingagepopulation.csv', low_memory=False)
 for index, row in data.iterrows():
     countryname = row[0]
     countrycode = row[1]
-    year_1990 = row[4]
-    year_1991 = row[5]
-    year_1992 = row[6]
-    year_1993 = row[7]
-    year_1994 = row[8]
-    year_1995 = row[9]
-    year_1996 = row[10]
-    year_1997 = row[11]
-    year_1998 = row[12]
-    year_1999 = row[13]
-    year_2000 = row[14]
-    year_2001 = row[15]
-    year_2002 = row[16]
-    year_2003 = row[17]
-    year_2004 = row[18]
-    year_2005 = row[19]
-    year_2006 = row[20]
-    year_2007 = row[21]
-    year_2008 = row[22]
-    year_2009 = row[23]
-    year_2010 = row[24]
+    year_1990 = float(row[4]) if row[4] != '..' else None
+    year_1991 = float(row[5]) if row[5] != '..' else None
+    year_1992 = float(row[6]) if row[6] != '..' else None
+    year_1993 = float(row[7]) if row[7] != '..' else None
+    year_1994 = float(row[8]) if row[8] != '..' else None
+    year_1995 = float(row[9]) if row[9] != '..' else None
+    year_1996 = float(row[10]) if row[10] != '..' else None
+    year_1997 = float(row[11]) if row[11] != '..' else None
+    year_1998 = float(row[12]) if row[12] != '..' else None
+    year_1999 = float(row[13]) if row[13] != '..' else None
+    year_2000 = float(row[14]) if row[14] != '..' else None
+    year_2001 = float(row[15]) if row[15] != '..' else None
+    year_2002 = float(row[16]) if row[16] != '..' else None
+    year_2003 = float(row[17]) if row[17] != '..' else None
+    year_2004 = float(row[18]) if row[18] != '..' else None
+    year_2005 = float(row[19]) if row[19] != '..' else None
+    year_2006 = float(row[20]) if row[20] != '..' else None
+    year_2007 = float(row[21]) if row[21] != '..' else None
+    year_2008 = float(row[22]) if row[22] != '..' else None
+    year_2009 = float(row[23]) if row[23] != '..' else None
+    year_2010 = float(row[24]) if row[24] != '..' else None
 
     q = WorkingAgeRate(countryname = countryname, countrycode = countrycode, year_1990 = year_1990, \
     year_1991 = year_1991, year_1992 = year_1992, year_1993 = year_1993, \
